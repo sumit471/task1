9c360506eae827fea7b316cdef1451fead7c38cf
= <sumitpal90123@gmail.com>
2021-12-16 16:05:53 +0530

    added styling to table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import MainData  from "./Components/MainData";
-import {makeStyles} from "@material-ui/core";
 
-const useStyle = makeStyles ({
-  heading:{
-    alignItems:"center",
-    marginLeft:"40%"
-  }
-})
 function App() {
-  const classes=useStyle();
   return (
     <>
-     <h1 className={classes.heading}>Data from json file</h1>
+     <h1>Data fetched from given JSON file</h1>
     <MainData />
-    </>
-   
+    </>   
   );
 }
 
